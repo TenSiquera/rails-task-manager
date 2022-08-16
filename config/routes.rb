220e95a82185df936a880be6245473a2c28b0823
felipe siqueira <felipedesouzasiqueira@gmail.com>
2022-08-16 17:15:03 -0300

    dooone

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,38 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # Read all tasks
+  get "tasks", to: "tasks#index"
+
+  # Create a task
+  get "tasks/new", to: "tasks#new", as: :tasks_new
+  post "tasks", to: "tasks#create"
+
+  # Read one task
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # Update a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Delete a task
+  delete 'tasks/:id', to: 'tasks#destroy'
+
+  # # Read all tasks
+  # get 'tasks', to: 'tasks#index'
+
+  # # Create a task
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
+
+  # # Read one task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # # Update a task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # Delete a task
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
 end
